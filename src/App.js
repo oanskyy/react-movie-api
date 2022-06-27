2ed45544f3c855b0d5339e6bb4ab23cbc1827ed2
oanskyy <oana.anghelachi@gmail.com>
2022-06-28 01:15:00 +0200

    Send a http GET request

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: "Some Dummy Movie",
+  //     openingText: "This is the opening text of the movie",
+  //     releaseDate: "2021-05-18",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Some Dummy Movie 2",
+  //     openingText: "This is the second opening text of the movie",
+  //     releaseDate: "2021-05-19",
+  //   },
+  // ];
+
+  // Send a GET request when the button is clicked
+  const fetchMoviesHandler = () => {
+    // fetch("https://api-stg.jam-community.com/song/trending")
+    fetch("https://swapi.dev/films/")
+      // fetch("https://swapi.py4e.com/")
+      .then((response) => {
+        console.log(response.json());
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        console.log(data.results);
+        setMovies(transformedMovies);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
