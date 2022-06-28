6ea4e2d7c9f1f62940ee6e94a51245053642d97e
oanskyy <oana.anghelachi@gmail.com>
2022-06-28 15:37:22 +0200

    Handle HTTP errors - refactor

--- src/App.js
+++ src/App.js
@@ -37,16 +37,27 @@ function App() {
     setIsLoading(false);
   }
 
+  let content =  <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error.message}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {!isLoading && error && <p>{error.message}</p>}
-        {isLoading && <p>Loading...</p>}
+      <section>{content}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />} */}
+        {/* {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>} */}
+        {/* {!isLoading && error && <p>{error.message}</p>} */}
+        {/* {isLoading && <p>Loading...</p>} */}
       </section>
     </React.Fragment>
   );
